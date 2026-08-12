18e9b0a202d650df7e4a3e48b260801e9759f4b0
davi-sehnem <davi_sehnem@estudante.sesisenai.org.br>
2026-08-12 15:18:05 -0300

    feat - prepared statement em cadastrar

--- public/cadastrar.php
+++ public/cadastrar.php
@@ -6,9 +6,15 @@ $titulo = $_POST["titulo"];
 $autor = $_POST["autor"];
 $ano = $_POST["ano"];
 
-$sql = "INSERT INTO livros (titulo,autor,ano) VALUES ('$titulo','$autor','$ano')";
+$sql = "INSERT INTO livros (titulo, autor, ano) VALUES (?, ?, ?)";
 
-mysqli_query($conexao, $sql);
+$stmt = mysqli_prepare($conexao, $sql);
+
+mysqli_stmt_bind_param($stmt, "ssi", $titulo, $autor, $ano);
+
+mysqli_stmt_execute($stmt);
+
+mysqli_stmt_close($stmt);
 
 header("Location: ../index.php");
 ?>
