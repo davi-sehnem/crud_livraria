f24ba84aa553b9c68769acce32c9d9f45ba9cc14
davi-sehnem <davi_sehnem@estudante.sesisenai.org.br>
2026-08-12 15:20:10 -0300

    feat - prepared statement em excluir

--- public/excluir.php
+++ public/excluir.php
@@ -1,7 +1,19 @@
 <?php
+
 include "../infra/conexao.php";
+
 $id = $_GET["id"];
-$sql = "DELETE FROM livros WHERE id=$id";
-mysqli_query($conexao,$sql);
+
+$sql = "DELETE FROM livros WHERE id = ?";
+
+$stmt = mysqli_prepare($conexao, $sql);
+
+mysqli_stmt_bind_param($stmt, "i", $id);
+
+mysqli_stmt_execute($stmt);
+
+mysqli_stmt_close($stmt);
+
 header("Location: ../index.php");
+
 ?>
