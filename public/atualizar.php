5f35551ae13cf1b1afb26e22f930f1ef916e77a7
davi-sehnem <davi_sehnem@estudante.sesisenai.org.br>
2026-08-12 15:17:02 -0300

    feat - prepared statement em atualizar

--- public/atualizar.php
+++ public/atualizar.php
@@ -7,7 +7,16 @@ $titulo = $_POST["titulo"];
 $autor = $_POST["autor"];
 $ano = $_POST["ano"];
 
-$sql = "UPDATE livros SET titulo='$titulo',autor='$autor',ano='$ano' WHERE id = '$id'";
+$sql = "UPDATE livros 
+        SET titulo = ?, autor = ?, ano = ? 
+        WHERE id = ?";
 
-mysqli_query($conexao, $sql);
+$stmt = mysqli_prepare($conexao, $sql);
+
+mysqli_stmt_bind_param($stmt, "ssii", $titulo, $autor, $ano, $id);
+
+mysqli_stmt_execute($stmt);
+
+mysqli_stmt_close($stmt);
+
 header("Location: ../index.php");
